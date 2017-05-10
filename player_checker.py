37f12e63cf743c5d31ab5c1d9dd829937f3941e7
Masatoshi Chang <masatoshi.chang@gmail.com>
2017-05-09 22:56:58 -0400

    Cleaned up code

--- player_checker.py
+++ player_checker.py
@@ -1,15 +1,28 @@
 import requests
-
-with open('local_data.txt', 'r') as f:
-    username = f.readline()
-    username = username.strip()
-
-url = "https://en.lichess.org/api/user/"+username
+import urllib.parse
 
 
-r = requests.get(url)
-total = r.json()["count"]["all"]
+PLAYER_BASE_URL = "https://en.lichess.org/api/user/"
 
-print("{} has played a total of {} games so far".format(username,total))
 
+class Player(object):
+
+    def __init__(self, username):
+        self.player_url = urllib.parse.urljoin(PLAYER_BASE_URL, username)
+
+    def get_total_games(self):
+        r = requests.get(self.player_url)
+        total = r.json()["count"]["all"]
+        return total
+
+
+if __name__ == '__main__':
+    with open('local_data.txt', 'r') as f:
+        username = f.readline()
+        username = username.strip()
+
+    a_player = Player(username)
+    total_games = a_player.get_total_games()
+
+    print("{} has played a total of {} games so far".format(username, total_games))
 
